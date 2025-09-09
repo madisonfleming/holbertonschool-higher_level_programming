bd0c332a98d6126efad2e407d1764cc86abb4184
root <maddyyfleming@gmail.com>
2025-09-09 13:29:37 +1000

    check

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    char = sentence[0]
-    if sentence == 0:
-        char = 0
+    if length == 0:
+        char = None
+    else:
+        char = sentence[0]
     return length, char
