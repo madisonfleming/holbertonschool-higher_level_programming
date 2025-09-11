b23b72cb1eacb3c1a53c8d396a53c3561441feba
root <maddyyfleming@gmail.com>
2025-09-11 14:34:37 +1000

    6

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+def print_sorted_dictionary(a_dictionary):
+    th_key = list(a_dictionary.keys())
+    th_key.sort()
+    sorted_dict = {i: a_dictionary[i] for i in th_key}
+    for key, value in sorted_dict.items():
+        print(f"{key}: {value}")
