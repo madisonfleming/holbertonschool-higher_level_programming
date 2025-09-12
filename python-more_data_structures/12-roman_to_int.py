a2d2fd3692ddde0b49fa5c1b7a8238301c6fde09
root <maddyyfleming@gmail.com>
2025-09-12 11:38:04 +1000

    12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+    roman_mp = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    i = 0
+    while i < len(roman_string):
+        if i + 1 < len(roman_string) and roman_mp[roman_string[i]] < \
+                roman_mp[roman_string[i + 1]]:
+            result += roman_mp[roman_string[i + 1]] - \
+                roman_mp[roman_string[i]]
+            i += 1
+        else:
+            result += roman_mp[roman_string[i]]
+        i += 1
+    return result
