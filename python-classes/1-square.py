cfa68a016847c056d7933149b3f276cef99cfa08
root <maddyyfleming@gmail.com>
2025-09-18 15:14:52 +1000

    1

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This is an square class
+"""
+
+
+class Square:
+    """A square class
+
+    Attributes:
+        size: size of the square
+    """
+    __size = 0
+
+    def __init__(self, size):
+        self.__size = size
