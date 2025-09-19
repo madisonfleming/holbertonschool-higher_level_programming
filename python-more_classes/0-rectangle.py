b29f84d3e09da796fa0cefdb366cc7e4bce959d3
root <maddyyfleming@gmail.com>
2025-09-19 12:18:38 +1000

    0

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This is an empty rectangle class
+"""
+
+
+class Rectangle:
+    """
+    Rectangle class
+    """
+    pass
---
+++ python-more_classes/README.md
@@ -0,0 +1,1 @@
+this is my more classes readme !
