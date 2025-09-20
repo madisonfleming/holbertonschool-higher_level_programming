0cb83c374091c0d53acb614b78710c8a467e59ed
root <maddyyfleming@gmail.com>
2025-09-20 14:19:51 +1000

    changeperimeterreturn

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -48,5 +48,5 @@ class Rectangle:
 
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
-            self.perimeter = 0
+            return 0
         return 2 * (self.__width + self.__height)
