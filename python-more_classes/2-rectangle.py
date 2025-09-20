4caa4cb0e496c5e5f01d4b981a55a50c328a86a5
root <maddyyfleming@gmail.com>
2025-09-20 14:17:36 +1000

    2

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""This is a rectangle class
+"""
+
+
+class Rectangle:
+    """
+    Rectangle class
+
+    Args:
+        width: width of the rectangle
+        height: height of the rectangle
+
+    Raises:
+        TypeError: if width or height are not integers
+        ValueError: if width or height are less than 0
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            self.perimeter = 0
+        return 2 * (self.__width + self.__height)
