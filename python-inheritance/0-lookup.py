1288c03ccd16db17c82ed654958c33e9e8ab6207
root <maddyyfleming@gmail.com>
2025-09-22 13:36:27 +1000

    document

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+class object:
+    """
+    This is a parent class
 
+    Attributes:
+        my_attr1: single attribute
+    """
 def lookup(obj):
+    """
+    A new instance of object to return the
+    attributes and methods of a class
+
+    Args:
+        obj: the object to lookup
+    """
     return dir(obj)
