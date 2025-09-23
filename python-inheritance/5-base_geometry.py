466a3e44b291257853e65d1708a05d9b9935fe81
root <maddyyfleming@gmail.com>
2025-09-23 12:12:16 +1000

    document

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""
+This module will create an empty class
+"""
+
+
 class BaseGeometry:
     """
-    This module will create an empty class
+    This is an empty class
     """
 
     pass
