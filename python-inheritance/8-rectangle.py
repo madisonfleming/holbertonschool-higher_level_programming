6ac723c738caedcd358f656f2d9e8577b44e9e36
root <maddyyfleming@gmail.com>
2025-09-23 18:57:34 +1000

    8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 """
-This module includes a subclass to BaseGeometry
+This module includes a parent class and subclass
 """
+
+
+class BaseGeometry:
+    """
+    This is a BaseGeometry class
+
+    Raises:
+        Exception: area() is not implemented
+    """
+
 
 class Rectangle(BaseGeometry):
     """
@@ -11,6 +21,13 @@ class Rectangle(BaseGeometry):
         height: private attribute
     """
     def __init__(self, width, height):
+        """
+        Initialises a new instance of Rectangle
+
+        Args:
+            width: int value
+            height: int value
+        """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
