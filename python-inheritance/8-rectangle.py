71a4331b203f9b8faa36073bf763633f1b0610f9
root <maddyyfleming@gmail.com>
2025-09-23 18:50:59 +1000

    8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,26 +1,7 @@
 #!/usr/bin/python3
 """
-This module includes a parent class and subclass
+This module includes a subclass to BaseGeometry
 """
-
-
-class BaseGeometry:
-    """
-    This is a BaseGeometry class
-
-    Raises:
-        Exception: area() is not implemented
-    """
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
 
 class Rectangle(BaseGeometry):
     """
