12e69e497d42f96de91020c53beec31e46a98890
root <maddyyfleming@gmail.com>
2025-09-24 14:28:29 +1000

    9

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This module imports a parent class and creates a subclass
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    This is a subclass of Rectangle
+    Args:
+        size: private attribute
+    """
+    def __init__(self, size):
+        """
+        Initialises a new instance of Square
+
+        Args:
+            size: int value
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return ("[Rectangle] {}/{}".format(self.__size, self.__size))
