1b54d644417caa02c7b0a9edfa821a85d2805576
root <maddyyfleming@gmail.com>
2025-09-25 17:49:52 +1000

    5

---
+++ python-abc/task_05_dragon.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This module will includes two parent classes or
+mixins and one subclass
+"""
+
+
+class SwimMixin:
+    """
+    """
+    def swim(self):
+        print("The creature swims!")
+
+
+class FlyMixin:
+    """
+    """
+    def fly(self):
+        print("The creature flies!")
+
+
+class Dragon(SwimMixin, FlyMixin):
+    """
+    """
+    def roar(self):
+        print("The drago roars!")
