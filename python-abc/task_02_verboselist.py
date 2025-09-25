201e07b235865ba0340a21c0aed937604da7be6d
root <maddyyfleming@gmail.com>
2025-09-25 15:29:18 +1000

    addedcheck

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -9,21 +9,23 @@ class VerboseList(list):
     """
     """
 
-    def __init__(self, ml):
+    def __init__(self, ml=None):
         super().__init__(ml if ml is not None else [])
-        self.ml = ml
 
-    def append(self, item):
-        super().append(item)
-        print("Added [{}] to the list".format(item))
+    def append(self, item=None):
+        if item is not None:
+            super().append(item)
+            print("Added [{}] to the list".format(item))
 
-    def extend(self, item):
-        super().extend(item)
-        print("Extended the list with [{}] items".format(len(item)))
+    def extend(self, item=None):
+        if item is not None:
+            super().extend(item)
+            print("Extended the list with [{}] items".format(len(item)))
 
-    def remove(self, item):
-        super().remove(item)
-        print("Removed [{}] from the list".format(item))
+    def remove(self, item=None):
+        if item is not None:
+            super().remove(item)
+            print("Removed [{}] from the list".format(item))
 
     def pop(self, index=-1):
         new_pop = super().pop(index)
