cf99becf0c48ce6735d3a938e5f20675333030ca
root <maddyyfleming@gmail.com>
2025-09-29 14:31:47 +1000

    4

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module return an object
+represented by a json string
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    """
+    """
+    This function returns an object
+    representated by a json string
+    """
+    list = json.loads(my_str)
+    return list
