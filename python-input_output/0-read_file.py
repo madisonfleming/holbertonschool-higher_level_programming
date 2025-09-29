af6036072227c0aaaae848e0e6144903b30f04ba
root <maddyyfleming@gmail.com>
2025-09-29 12:42:15 +1000

    document!

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""
+This module reads a text file and prints it
+"""
+
+
 def read_file(filename=""):
+    """
+    This function will open and read a file
+    and print line by line
+    """
     with open("my_file_0.txt", "r", encoding="utf-8") as f:
         for line in f:
             print(line, end="")
