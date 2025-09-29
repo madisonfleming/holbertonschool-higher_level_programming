510a29cdba2e916812907ac924aab12a82ace129
root <maddyyfleming@gmail.com>
2025-09-29 13:41:58 +1000

    0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,6 +9,6 @@ def read_file(filename=""):
     This function will open and read a file
     and print line by line
     """
-    with open("my_file_0.txt", "r", encoding="utf-8") as filename:
-        line = filename.read()
-        print(line, end="")
+    with open(filename, "r", encoding="utf-8") as f:
+        for line in f:
+            print(line, end="")
