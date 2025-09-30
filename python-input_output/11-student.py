863173b46c7b04f91c6fe878f6400b42e900a47b
root <maddyyfleming@gmail.com>
2025-09-30 15:28:10 +1000

    11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This module returns the dictionary description
+with simple data structure for a json
+serialisation of an object
+"""
+
+
+class Student:
+    """
+    This is a Student class
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        this function will return a structure for a
+        json serialisation of an object
+
+        Attributes:
+            attrs: if a list of strings, only
+            attribute names must be retrieved,
+            otherwise retrieve all attributes
+        """
+        if isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        self.first_name = "John"
+        self.last_name = "Doe"
+        self.age = "23"
