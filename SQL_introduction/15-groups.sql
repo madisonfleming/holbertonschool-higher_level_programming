c804fa515d72f3e06f4525bac7448cf54ef3400d
madisonfleming <maddyyfleming@gmail.com>
2025-10-20 15:54:08 +1100

    15

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- list the number of records with the same score
-SELECT score, COUNT(*) AS number FROM second_table GROUP BY score HAVING COUNT(*)
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score HAVING COUNT(*) ORDER BY number DESC
