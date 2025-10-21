ef04a8a436f85c81be70a32a3afb95d446a750aa
madisonfleming <maddyyfleming@gmail.com>
2025-10-22 09:59:29 +1100

    14

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- List all genres of the show Dexter
+SELECT DISTINCT (name)
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_shows.title = "Dexter"
+ORDER BY name ASC
