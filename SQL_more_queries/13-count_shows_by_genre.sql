300b07416052b2bb6afd7b8ce8cede1e7439fa5d
madisonfleming <maddyyfleming@gmail.com>
2025-10-22 09:20:55 +1100

    13

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- list all genres and display number of shows linked 
+SELECT name AS genre, COUNT(tv_show_genres.show_id) as number_of_shows
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY name
+ORDER BY number_of_shows DESC
