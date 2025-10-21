72cf6f919e6de81065d28ba42f1ded4cb194a652
madisonfleming <maddyyfleming@gmail.com>
2025-10-21 17:46:13 +1100

    8

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,5 @@
 -- List all the cities in the database
-SELECT cities.id, cities.name, states.name FROM cities, states ORDER BY cities.id ASC
+SELECT cities.id, cities.name, states.name 
+FROM cities, states
+WHERE (cities.id = states.id)
+ORDER BY cities.id ASC 
