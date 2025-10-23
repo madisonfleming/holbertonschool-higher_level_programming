48f9812190d505399d52dd3230f279059bda5321
madisonfleming <maddyyfleming@gmail.com>
2025-10-23 16:56:10 +1100

    0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,8 +2,9 @@
 """ List all states from the database """
 if __name__ == "__main__":
     import MySQLdb
+    import sys
 
-    db = MySQLdb.connect(host="localhost", port=3306, database="hbtn_0e_0_usa")
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], password=sys.argv[2], database=sys.argv[3])
     cursor = db.cursor()
     cursor.execute("""SELECT * FROM states""")
     result = cursor.fetchall()
