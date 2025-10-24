a0a11ab8ba3ea7310e6d5971885c01657f1de6d8
madisonfleming <maddyyfleming@gmail.com>
2025-10-24 18:47:50 +1100

    10

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""List all State objects with the name
+passed as argument from the database using SQLalchemy
+"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    state_name = sys.argv[4]
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for a in sys.argv:
+        if a.count(";"):
+            exit()
+    instance = session.query(State).where(State.name == state_name)
+    if instance.count() == 0:
+        print("Not found")
+    else:
+        for state in instance:
+            print("{}".format(state.id))
+
+"""
+    instance = session.query(State).first()
+    if instance is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(instance.id, instance.name))
+"""
