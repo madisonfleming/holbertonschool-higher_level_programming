c60818dfc21e28b19aa5fc0033bfb22ae91af199
madisonfleming <maddyyfleming@gmail.com>
2025-10-24 14:12:32 +1100

    2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" List all states from the database where name starts with N """
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         password=sys.argv[2], database=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("""SELECT * FROM states
+                   WHERE name = '{}'
+                   ORDER BY id ASC""".format(sys.argv[4]))
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
