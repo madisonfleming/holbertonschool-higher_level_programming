dc0b21a985a4eaa76a4aa92fdd1bca6259871413
madisonfleming <maddyyfleming@gmail.com>
2025-11-13 10:25:33 +1100

    0

---
+++ javascript-dom_manipulation/0-script.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+function updateHeader () {
+  const header = document.querySelector('header');
+  header.style.backgroundColor = '#FF0000';
+}
+updateHeader();
---
+++ javascript-dom_manipulation/README.md
@@ -0,0 +1,1 @@
+this is my java dom manipulation readme wish me luck
